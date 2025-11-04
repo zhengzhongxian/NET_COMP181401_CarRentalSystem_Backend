59fc47b5dd8de1abab141ae97ead2ab0d20ac5f0
Lam Tieu Minh <minhlam1610.work@gmail.com>
2025-11-04 15:07:57 +0700

    * refactor API & CRUD Vehicle_Category 3rd time

--- NET_CarRentalSystem.Application/Features/VehicleCategories/Queries/GetAllVehicleCategories/GetAllVehicleCategoriesQuery.cs
+++ NET_CarRentalSystem.Application/Features/VehicleCategories/Queries/GetAllVehicleCategories/GetAllVehicleCategoriesQuery.cs
@@ -1,9 +1,31 @@
 using MediatR;
+using NET_CarRentalSystem.Application.Common.Interfaces.CQRS;
 using NET_CarRentalSystem.Application.DTOs.VehicleCategoryDTOs.Get;
+using NET_CarRentalSystem.Domain.Entities;
+using NET_CarRentalSystem.Domain.Interfaces.Persistence;
 
 namespace NET_CarRentalSystem.Application.Features.VehicleCategories.Queries.GetAllVehicleCategories;
 
-public class GetAllVehicleCategoriesQuery : IRequest<List<GetVehicleCategoryDto>>
+public class GetAllVehicleCategoriesQuery : IQuery<List<GetVehicleCategoryDto>>
 {
-    
+}
+
+public class GetAllVehicleCategoriesQueryHandler(IUnitOfWork unitOfWork)
+    : IRequestHandler<GetAllVehicleCategoriesQuery, List<GetVehicleCategoryDto>>
+{
+    public async Task<List<GetVehicleCategoryDto>> Handle(
+        GetAllVehicleCategoriesQuery request,
+        CancellationToken cancellationToken)
+    {
+        var categories = await unitOfWork.GetRepository<VehicleCategory>().GetAllAsync(cancellationToken);
+
+        var categoryDtos = categories.Select(category => new GetVehicleCategoryDto
+        {
+            CategoryId = category.CategoryId,
+            CategoryCode = category.CategoryCode,
+            Seat = category.Seat
+        }).ToList();
+
+        return categoryDtos;
+    }
 }
